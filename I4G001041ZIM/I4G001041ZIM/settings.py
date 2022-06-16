6798c92a1e1f7a0d0a20e1fb513674f50937dd1c
Gloria <ubahgloria16@gmail.com>
2022-06-16 16:42:17 +0100

    new model created

--- I4G001041ZIM/I4G001041ZIM/settings.py
+++ I4G001041ZIM/I4G001041ZIM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h(e#9-+^6t2q)d(4rht)@#)3!!jywu7k0l+-c=4&h*opnq^l&x'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
